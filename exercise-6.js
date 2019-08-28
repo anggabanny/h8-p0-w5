3e948ac4789a03cbb030492a69000312d65363c2
anggabanny <anggabanny@gmail.com>
2019-08-28 19:12:21 +0700

    💯⤴️

--- exercise-6.js
+++ exercise-6.js
@@ -46,7 +46,6 @@ function meleeRangedGrouping(str) {
 }
 
 // TEST CASE
-
 console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
 // [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
 
